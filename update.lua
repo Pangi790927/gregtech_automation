61cb06f6b89e097d36b0b56e6c1285238f5320e2
Andrei Pangratie <andrpangr@gmail.com>
2024-05-01 02:00:08 +0300

    update

--- update.lua
+++ update.lua
@@ -1,19 +1,17 @@
 shell = require("shell")
 
-file_list = {
-	"gtnh_crafter.lua",
-	"gtnh_reciper.lua",
-	"helpers.lua",
-	"hw_crafting.lua",
-	"hw_interface.lua",
-	"item_helper.lua",
-	"recipe_db.lua",
-	"cleanroom.lua",
-	"main.lua",
-	"update.lua"
-}
+file_list = { "gtnh_crafter.lua", "gtnh_reciper.lua" }
+-- 	"helpers.lua",
+-- 	"hw_crafting.lua",
+-- 	"hw_interface.lua",
+-- 	"item_helper.lua",
+-- 	"recipe_db.lua",
+-- 	"cleanroom.lua",
+-- 	"main.lua",
+-- 	"update.lua"
+-- }
 
-for f in file_list do
+for k,f in ipairs(file_list) do
 	shell.execute("rm " .. f)
 	shell.execute("wget https://raw.githubusercontent.com/Pangi790927/gregtech_automation/main/" .. f)
 end
